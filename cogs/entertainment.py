5feb53d3f57a753200c73feaf0f531d25841cd4e
V. Karch <luna.karch05@Gmail.com>
2023-08-31 15:46:37 -0400

    Added the entertainment cog in it's barebones format

---
+++ cogs/entertainment.py
@@ -0,0 +1,11 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+class Entertainment(commands.Cog):
+    def __init__(self, client: commands.Bot) -> None:
+        self.client = client
+
+
+async def setup(client: commands.Bot) -> None:
+    await client.add_cog(Entertainment(client))
