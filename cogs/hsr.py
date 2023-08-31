1406f548c795ecf3bf6db766885ae3020e14bbb3
V. Karch <luna.karch05@Gmail.com>
2023-08-31 12:08:00 -0400

    Updated hsr.py with better typehinting and nicer comments

--- cogs/hsr.py
+++ cogs/hsr.py
@@ -2,16 +2,19 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
+
 class HSR(commands.Cog):
     """
     Honkai: Star Rail Interaction Commands Cog
     Houses all commands relating to Honkai: Star Rail
     """
-    def __init__(self, client: commands.Bot):
+
+    def __init__(self, client: commands.Bot) -> None:
         self.client: commands.Bot = client
+        # ^^ Sets the client to be an attribute of the class
 
 
-async def setup(client: commands.Bot):
+async def setup(client: commands.Bot) -> None:
     """
     Cog Setup Function, required for every cog that needs to be loaded.
     Adds all the commands in the cog to the client and loads them
