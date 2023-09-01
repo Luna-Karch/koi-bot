ad73d8df180b957950f129f18dd7b6abedd292ff
Luna <luna.karch05@gmail.com>
2023-08-31 23:35:35 -0400

    fixed the typo in the typehinting. lol

--- cogs/entertainment.py
+++ cogs/entertainment.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 
 class Entertainment(commands.Cog):
     def __init__(self, client: commands.Bot) -> None:
-        self.client: Bot = client
+        self.client: commands.Bot = client
 
 
 async def setup(client: commands.Bot) -> None:
