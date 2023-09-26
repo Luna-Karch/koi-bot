8d3afe3f59bdf0ea4a8367932623ebadbb3360b8
Luna-Karch <luna.karch05@gmail.com>
2023-09-26 12:10:01 -0400

    Added basic information, implemented the CharacterDropdown Selection Menu and added it to the view

--- models/player_card_view.py
+++ models/player_card_view.py
@@ -1,5 +1,11 @@
 from discord.ui import View
+from models.character_dropdown import CharacterDropdown
+from mihomo.models import StarrailInfoParsed
 
 
 class PlayerCardView(View):
-    ...
+    def __init__(self, user_id: int, hsr_info: StarrailInfoParsed):
+        self.user_id = user_id
+        self.hsr_info = hsr_info
+        super().__init__(timeout=None)
+        self.add_item(CharacterDropdown(user_id, hsr_info))
