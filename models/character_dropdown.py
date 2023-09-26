02e7c92eb87440e7a384018881b7b2a10902f1c1
Luna-Karch <luna.karch05@gmail.com>
2023-09-26 12:09:23 -0400

    Updated the template for the selection dropdown menu, much more to be added

--- models/character_dropdown.py
+++ models/character_dropdown.py
@@ -1,5 +1,13 @@
 from discord.ui import Select
+from mihomo.models import StarrailInfoParsed
 
 
 class CharacterDropdown(Select):
-    ...
+    def __init__(self, user_id: int, hsr_info: StarrailInfoParsed):
+        options = ...
+        super().__init__(
+            placeholder="Select a character",
+            max_values=1,
+            min_values=1,
+            options=options,
+        )
