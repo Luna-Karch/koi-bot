594d50a45773d98a9e2d00383b519ddeec700e77
Luna Karch <luna.karch05@gmail.com>
2024-07-25 14:22:05 -0400

    retroachievements: fix indentation levels

--- cogs/retroachievements.py
+++ cogs/retroachievements.py
@@ -7,10 +7,10 @@ class Retroachievements(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client: commands.Bot  = client
 
-   @app_commands.command(name = "retro-profile", description = "get a users profile from retroachievements if it exists") 
-   @app_commands.describe(username = "The retroachievements username of the person you want to lookup")
-   async def retro_profile(self, interaction: discord.Interaction, username: str):
-       await interaction.response.send_message("This is a placeholder command!")
+    @app_commands.command(name = "retro-profile", description = "get a users profile from retroachievements if it exists") 
+    @app_commands.describe(username = "The retroachievements username of the person you want to lookup")
+    async def retro_profile(self, interaction: discord.Interaction, username: str):
+        await interaction.response.send_message("This is a placeholder command!")
 
 async def setup(client: commands.Bot) -> None:
     client.add_cog(Retroachievements(client))
